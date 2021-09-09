be73f8f2353bbd732d4c754c267c27878609d705
resposito0925 <ramirodesposito@gmail.com>
2021-09-09 20:31:24 -0300

    Ejercicio 2: ingreso de texto

---
+++ Ejercicio 2/Ejercicio 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido, es un placer saludarle");
+
+            Console.WriteLine("Ingrese un texto cualquiera y presione ENTER, por favor");
+            string ingreso = Console.ReadLine();
+
+            Console.WriteLine("Usted ha ingresado: " + ingreso);
+
+            Console.ReadKey();
+        }
+    }
+}
